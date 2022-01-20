78fe869f693d41da9eff8d6bbc54aa503e197330
BryptoMensch <90989763+BryptoMensch@users.noreply.github.com>
2022-01-20 13:48:45 -0500

    Delete d2 directory

--- d2/timeout/timer1.js
+++
@@ -1,8 +0,0 @@
-const time = process.argv.slice(2);
-for (let index of time) {
-	index = Number(index);
-	if (typeof index === "number" && index > 0) {
-		setTimeout(() => process.stdout.write(","), index * 1000);
-	}
-
-}
